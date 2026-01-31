0ee6808866df87f3542311398508c878efbccd9a
babangidaaminuonline-ux <babangidaaminuonline@gmail.com>
2026-01-31 07:33:56 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
 import AdminDashboard from './pages/AdminDashboard';
